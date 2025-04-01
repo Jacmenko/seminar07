3ec1f0b3d8ab428b68fde03cd463aeefd66a6e6a
Lukáš Myjavec <xmyjavec@fi.muni.cz>
2025-04-01 19:49:13 +0200

    feat: added tests

---
+++ TestSeminar07/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestSeminar07
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
